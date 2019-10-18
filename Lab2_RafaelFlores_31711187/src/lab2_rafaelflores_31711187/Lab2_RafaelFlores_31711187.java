9b75b319914953d2d2d65c1ae5d2475220a8dd81
Rafael <rafac@unitec.edu>
2019-10-18 14:06:44 -0600

    Login implementado

--- Lab2_RafaelFlores_31711187/src/lab2_rafaelflores_31711187/Lab2_RafaelFlores_31711187.java
+++ Lab2_RafaelFlores_31711187/src/lab2_rafaelflores_31711187/Lab2_RafaelFlores_31711187.java
@@ -1,21 +1,67 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Rafael Flores - Lab 2
  */
 package lab2_rafaelflores_31711187;
+import java.util.Scanner;
 
-/**
- *
- * @author r4f4
- */
 public class Lab2_RafaelFlores_31711187 {
-
-    /**
-     * @param args the command line arguments
-     */
+    public static Scanner read = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        boolean log = false;
+        while (true) {            
+            System.out.println("¡Bienvenido!");
+            System.out.println("Ingrese la opción de su elección:"
+                    + "\n\t 1. Crear Universidad"
+                    + "\n\t 2. Eliminar Universidad"
+                    + "\n\t 3. Log In"
+                    + "\n\t 4. Ascender Universidad"
+                    + "\n\t 5. Listar Universidad y su Sucursal"
+                    + "\n\t 6. Modificar"
+                    + "\n\t 7. Descender Universidad"
+                    + "\n\t 8. Random"
+                    + "\n\t 0. Salir");
+            int op = read.nextInt();
+            if (op == 0) {
+                System.exit(0);
+            }
+            if (!log && (op != 3)) {
+                System.out.println("Debe logearse primero, antes de usar el progrma");
+            }else{
+                switch(op){
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        if (!log) {
+                            System.out.println("Ingrese su usuario: ");
+                            String user = read.next();
+                            System.out.println("Ingrese su contraseña: ");
+                            String pass = read.next();
+                            if (user.equals("leobanegas") && pass.equals("99")) {
+                                System.out.println("Bienvenido a la Sistema de Gestión de Uniersidades Leonardo!");
+                            } else {
+                                System.out.println("Su usuario o contraseña son incorrectos!");
+                            }
+                            log = true;
+                        }else{
+                            System.out.println("Usted ya está logueado!!!");
+                        }
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                }
+            }
+        }
     }
     
 }
--- Lab2_RafaelFlores_31711187/src/lab2_rafaelflores_31711187/Universidad.java
+++ Lab2_RafaelFlores_31711187/src/lab2_rafaelflores_31711187/Universidad.java
@@ -1,14 +1,93 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Clase Universidad
  */
 package lab2_rafaelflores_31711187;
 
-/**
- *
- * @author r4f4
- */
 public class Universidad {
+    private String nombre, nombreRector, sucursal, nivel;
+    private int añoCreacion, numMaestro, numEstudiante; 
+    private float costo;
+
+    public Universidad(String nombre, String nombreRector, String sucursal, String nivel, int añoCreacion, int numMaestro, int numEstudiante, float costo) {
+        this.nombre = nombre;
+        this.nombreRector = nombreRector;
+        this.sucursal = sucursal;
+        this.nivel = nivel;
+        this.añoCreacion = añoCreacion;
+        this.numMaestro = numMaestro;
+        this.numEstudiante = numEstudiante;
+        this.costo = costo;
+    }
     
+    public Universidad(){}
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNombreRector(String nombreRector) {
+        this.nombreRector = nombreRector;
+    }
+
+    public void setSucursal(String sucursal) {
+        this.sucursal = sucursal;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public void setAñoCreacion(int añoCreacion) {
+        this.añoCreacion = añoCreacion;
+    }
+
+    public void setNumMaestro(int numMaestro) {
+        this.numMaestro = numMaestro;
+    }
+
+    public void setNumEstudiante(int numEstudiante) {
+        this.numEstudiante = numEstudiante;
+    }
+
+    public void setCosto(float costo) {
+        this.costo = costo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNombreRector() {
+        return nombreRector;
+    }
+
+    public String getSucursal() {
+        return sucursal;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public int getAñoCreacion() {
+        return añoCreacion;
+    }
+
+    public int getNumMaestro() {
+        return numMaestro;
+    }
+
+    public int getNumEstudiante() {
+        return numEstudiante;
+    }
+
+    public float getCosto() {
+        return costo;
+    }
+    
+    @Override
+    public String toString(){
+        return "| "+this.getNombre()+" | "+this.getNombreRector()+" | "+this.getSucursal()+" | "+this.getAñoCreacion()+" | "+this.getNumMaestro()+" | "+this.getNumEstudiante()+" | "+this.getCosto()+" | "+this.getNivel()+" |";
+    }
 }
+    
